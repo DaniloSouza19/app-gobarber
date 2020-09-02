6225372fdb33aa2c248c2e2c6d4b0463ce091c91
Danilo Souza <danilo_souza19@outlook.com>
2020-09-02 15:45:52 -0300

    add fonts/assets

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
 
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Text>SignIn</Text>
+      <Image source={logo} />
     </Container>
   );
 };
